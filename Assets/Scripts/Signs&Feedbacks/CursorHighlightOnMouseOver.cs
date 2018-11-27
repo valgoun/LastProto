8dd340acab743e1b3eb287bc231c3827536b5020
Own3dbysquirrel <own3dbysquirrel@gmail.com>
2018-11-27 15:57:17 +0100

    Camera Tweaks & WIP cursor script

---
+++ Assets/Scripts/Signs&Feedbacks/CursorHighlightOnMouseOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHighlightOnMouseOver : MonoBehaviour {
+
+    public Color cursorHighlightColor;
+    public Texture2D newCursor;
+
+   void OnMouseOver()
+    {
+        Cursor.SetCursor(newCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
