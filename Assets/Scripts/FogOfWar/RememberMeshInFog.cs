fdcc4003e62a83faabaf1700b4e8eb4dec88b519
Unknown <valgoun@gmail.com>
2018-11-29 16:52:38 +0100

    Remember Mesh

---
+++ Assets/Scripts/FogOfWar/RememberMeshInFog.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RememberMeshInFog : MonoBehaviour, IFogVisionNotifiable
+{
+    public bool IsVisible
+    {
+        get
+        {
+            return _isVisible;
+        }
+
+        set
+        {
+            _isVisible = value;
+        }
+    }
+
+    private bool _isVisible;
+    private MeshRenderer _renderer;
+
+    public void OnVisionEnter()
+    {
+        _renderer.enabled = _isVisible;
+    }
+
+    public void OnVisionExit()
+    {
+    }
+
+    private void Awake()
+    {
+        _isVisible = true;
+        _renderer = GetComponent<MeshRenderer>();
+        _renderer.enabled = false;
+    }
+}
