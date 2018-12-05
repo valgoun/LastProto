d0525508f063bf1014f72b35c08d7904234a47bd
Unknown <valgoun@gmail.com>
2018-12-05 16:49:53 +0100

    Fading Shader System

---
+++ Assets/Scripts/Utilities/FadingCloseToUnit/FadingObjectComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using System;
+using Unity.Mathematics;
+
+[Serializable]
+public struct FadingObject : IComponentData
+{
+    public float3 Position;
+}
+
+public class FadingObjectComponent : ComponentDataWrapper<FadingObject> {}
